6585c06126cb0b9dcba76fcf1eda024eb7c6c8bb
Nicky Kosasih <71074389+zukui1984@users.noreply.github.com>
2024-04-13 21:12:24 +0200

    Correcting main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,31 +4,14 @@ provider "google" {
   region      = "eu"
 }
 
-
 resource "google_storage_bucket" "data_bucket" {
   name     = "nyc_taxi_trips_2024"
-  location = "eu"
+  location = "EU"  # Change to EU, as per Google Cloud region codes
 }
 
 resource "google_bigquery_dataset" "data_warehouse" {
   dataset_id = "nyc_taxi_trips"
   location   = "US"
 }
+  
 
-resource "google_compute_instance" "data_vm" {
-  name         = "project-instance"
-  machine_type = "e2-medium"
-  zone         = "us-west1"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-10"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-    }
-  }
-}
